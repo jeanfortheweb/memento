4ee2639dc060798e3457f2bc007f772fcfc77e4c
jeanfortheweb <jeanfortheweb@gmail.com>
2018-02-14 21:32:46 +0100

    Code change

--- packages/react-todo-example/src/components/TodoList/Footer.jsx
+++ packages/react-todo-example/src/components/TodoList/Footer.jsx
@@ -3,29 +3,21 @@ import { Row, Col, Button, Input } from 'antd';
 import { View, Trigger } from '@memento/react';
 import todoStore, { addTodo, setTodoText, getTodoText } from '../../stores/todo';
 
-const AddButton = () => (
-  <Trigger store={todoStore} factory={addTodo}>
-    {onClick => <Button onClick={onClick}>Add Todo</Button>}
-  </Trigger>
-);
-
-const TextInput = () => (
-  <Trigger store={todoStore} factory={setTodoText}>
-    {onChange => (
-      <View store={todoStore} selector={getTodoText}>
-        {text => <Input value={text} onChange={onChange} placeholder="Enter todo text..." />}
-      </View>
-    )}
-  </Trigger>
-);
-
 const Footer = () => (
   <Row justify="end">
     <Col span={18}>
-      <TextInput />
+      <Trigger store={todoStore} factory={setTodoText}>
+        {onChange => (
+          <View store={todoStore} selector={getTodoText}>
+            {text => <Input value={text} onChange={onChange} placeholder="Enter todo text..." />}
+          </View>
+        )}
+      </Trigger>
     </Col>
     <Col offset={2} span={2}>
-      <AddButton />
+      <Trigger store={todoStore} factory={addTodo}>
+        {onClick => <Button onClick={onClick}>Add Todo</Button>}
+      </Trigger>
     </Col>
   </Row>
 );
