8b139e694f385c8d910431b263986e06c7af5d8c
jeanfortheweb <jeanfortheweb@gmail.com>
2018-02-14 21:31:50 +0100

    Add features

--- packages/react-todo-example/src/components/TodoList/Item.jsx
+++ packages/react-todo-example/src/components/TodoList/Item.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { List, Col, Row, Checkbox } from 'antd';
 import { Trigger } from '@memento/react';
-import todoStore, { addTodo } from '../../stores/todo';
+import todoStore, { toggleTodo } from '../../stores/todo';
 
 const Item = ({ data }) => (
   <List.Item>
     <Row align="middle" type="flex" style={{ width: '100%' }}>
       <Col span={2}>
-        <Checkbox />
+        <Trigger store={todoStore} factory={toggleTodo}>
+          {onClick => <Checkbox checked={data.done} onClick={() => onClick(data.id)} />}
+        </Trigger>
       </Col>
       <Col span={22}>{data.text}</Col>
     </Row>
--- packages/react-todo-example/src/stores/todo.js
+++ packages/react-todo-example/src/stores/todo.js
@@ -1,6 +1,8 @@
+import { Observable, withLatestFrom, flatMap } from '@reactivex/rxjs';
 import { Store } from '@memento/store';
 import { createStoreWorker, createSequenceWorker, sequence, push, merge } from '@memento/common';
 import { Record, List } from 'immutable';
+import shortid from 'shortid';
 
 export class Todo extends Record({
   id: null,
@@ -14,22 +16,37 @@ export class State extends Record({
 }) {}
 
 const todoWorker = (task$, state$) =>
-  task$
+  Observable.merge(
     // we could also do this by using sequence, push and set from @memento/common without
     // the need of a custom worker, but this way, we can pull the text to set from the state
     // so it doesn't have to be passed to addTodo().
-    .accept('@TODO/ADD')
-    .withLatestFrom(state$, (task, state) => state =>
-      state.update('todos', todos => todos.push(new Todo({ text: state.text }))).set('text', ''),
-    );
+    task$
+      .accept('@TODO/ADD')
+      .withLatestFrom(state$)
+      .map(([task, state]) =>
+        sequence(
+          push('todos', new Todo({ id: shortid.generate(), text: state.text })),
+          merge({ text: '' }),
+        ),
+      ),
+    task$
+      .accept('@TODO/TOGGLE')
+      .map(task => state =>
+        state.update('todos', todos =>
+          todos.update(todos.findKey(todo => todo.id === task.id), todo =>
+            todo.set('done', !todo.done),
+          ),
+        ),
+      ),
+  );
 
-export const addTodo = () => ({
-  kind: '@TODO/ADD',
-});
-
+// task creators
+export const addTodo = () => ({ kind: '@TODO/ADD' });
+export const toggleTodo = id => ({ kind: '@TODO/TOGGLE', id });
 export const setTodoText = event => merge({ text: event.target.value });
 
+// selectors
 export const getTodos = state => state.todos;
 export const getTodoText = state => state.text;
 
-export default new Store(new State(), [createStoreWorker(), todoWorker]);
+export default new Store(new State(), [createStoreWorker(), createSequenceWorker(), todoWorker]);
