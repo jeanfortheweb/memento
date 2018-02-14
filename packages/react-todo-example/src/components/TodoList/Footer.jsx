b58bc941fe1a4360b763adc90ae1b21038f8a5bc
jeanfortheweb <jeanfortheweb@gmail.com>
2018-02-14 21:34:43 +0100

    Remove div

--- packages/react-todo-example/src/components/TodoList/Footer.jsx
+++ packages/react-todo-example/src/components/TodoList/Footer.jsx
@@ -4,24 +4,22 @@ import { View, Trigger } from '@memento/react';
 import todoStore, { addTodo, setTodoText, getTodoText } from '../../stores/todo';
 
 const Footer = () => (
-  <div>
-    <Row justify="end">
-      <Col span={18}>
-        <Trigger store={todoStore} factory={setTodoText}>
-          {onChange => (
-            <View store={todoStore} selector={getTodoText}>
-              {text => <Input value={text} onChange={onChange} placeholder="Enter todo text..." />}
-            </View>
-          )}
-        </Trigger>
-      </Col>
-      <Col offset={2} span={2}>
-        <Trigger store={todoStore} factory={addTodo}>
-          {onClick => <Button onClick={onClick}>Add Todo</Button>}
-        </Trigger>
-      </Col>
-    </Row>
-  </div>
+  <Row justify="end">
+    <Col span={18}>
+      <Trigger store={todoStore} factory={setTodoText}>
+        {onChange => (
+          <View store={todoStore} selector={getTodoText}>
+            {text => <Input value={text} onChange={onChange} placeholder="Enter todo text..." />}
+          </View>
+        )}
+      </Trigger>
+    </Col>
+    <Col offset={2} span={2}>
+      <Trigger store={todoStore} factory={addTodo}>
+        {onClick => <Button onClick={onClick}>Add Todo</Button>}
+      </Trigger>
+    </Col>
+  </Row>
 );
 
 export default Footer;
