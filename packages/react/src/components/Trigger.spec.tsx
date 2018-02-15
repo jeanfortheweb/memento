3c41c89e9c004cead799c6d084967c84ed6f4571
jeanfortheweb <jeanfortheweb@gmail.com>
2018-02-15 16:43:00 +0100

    New Trigger implmentation

--- packages/react/src/components/Trigger.spec.tsx
+++ packages/react/src/components/Trigger.spec.tsx
@@ -3,7 +3,6 @@ import { Record } from 'immutable';
 import { configure, mount } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 import { Store } from '@memento/store';
-import { createStateUpdater, updateState } from '@memento/common';
 import Trigger from './Trigger';
 
 configure({ adapter: new Adapter() });
@@ -16,14 +15,14 @@ class State extends Record<StateProps>({
   text: '',
 }) {}
 
-const store = new Store(new State(), [createStateUpdater<State, StateProps>()]);
-const StringTrigger = Trigger.for<State, string>();
+const store = new Store(new State(), []);
+const StringTrigger = Trigger.for<State, { onClick: (text: string) => void }>();
 
-test('a trigger does call its factory on invocation', () => {
-  const render = jest.fn(onClick => <button onClick={() => onClick('Hello')}>button</button>);
-  const factory = jest.fn(text => updateState({ text }));
+test('a trigger does call its functions on invocation', () => {
+  const render = jest.fn(({ onClick }) => <button onClick={() => onClick('Hello')}>button</button>);
+  const factory = jest.fn();
   const wrapper = mount(
-    <StringTrigger store={store} factory={factory}>
+    <StringTrigger store={store} onClick={factory}>
       {render}
     </StringTrigger>,
   );
@@ -32,4 +31,22 @@ test('a trigger does call its factory on invocation', () => {
 
   expect(render).toHaveBeenCalledTimes(1);
   expect(factory).toHaveBeenCalledWith('Hello');
+});
+
+test('a trigger does update its functions on prop change', () => {
+  const render = jest.fn(({ onClick }) => <button onClick={() => onClick('Hello')}>button</button>);
+  const newHandler = jest.fn();
+  const handler = jest.fn();
+  const wrapper = mount(<StringTrigger store={store} onClick={handler} render={render} />);
+
+  wrapper.setProps({
+    onClick: newHandler,
+  });
+
+  wrapper.find('button').simulate('click');
+
+  expect(render).toHaveBeenCalledTimes(2);
+  expect(handler).toHaveBeenCalledTimes(0);
+  expect(newHandler).toHaveBeenCalledTimes(1);
+  expect(newHandler).toHaveBeenCalledWith('Hello');
 });
--- packages/react/src/components/Trigger.tsx
+++ packages/react/src/components/Trigger.tsx
@@ -1,30 +1,54 @@
 import * as React from 'react';
-import { Record } from 'immutable';
-import { Store, TaskFactory } from '@memento/store';
+import { Record, Map } from 'immutable';
+import { Store } from '@memento/store';
 import Render, { RenderFunction } from './Render';
 
-export interface Props<TState extends Record<any>, TParameters> {
+export interface Props<TState extends Record<any>, TProps> {
   store: Store<TState>;
-  factory: TaskFactory<TState, TParameters>;
-  children: RenderFunction<(parameters: TParameters) => void>;
+  render?: RenderFunction<TProps>;
+  children?: RenderFunction<TProps>;
+}
+
+export interface TriggerProps {
+  [key: string]: (...parameters: any[]) => void;
 }
 
 export default class Trigger<
   TProps extends Props<any, any> = Props<any, any>
 > extends React.PureComponent<TProps> {
-  public static for<TState extends Record<any>, TParameters>() {
+  public static for<TState extends Record<any>, TProps extends TriggerProps>() {
     return Trigger as {
-      new (props?: Props<TState, TParameters>): Trigger<Props<TState, TParameters>>;
+      new (props?: Props<TState, TProps> & TProps): Trigger<Props<TState, TProps> & TProps>;
     };
   }
 
-  handleTrigger = (parameters: any) => {
-    const { store, factory } = this.props;
+  _triggers = {};
 
-    store.assign(factory(parameters));
-  };
+  _bindTriggers(props: TProps) {
+    const { store } = this.props;
+
+    this._triggers = Object.keys(props).reduce((triggers, prop) => {
+      if (prop !== 'store' && prop !== 'render' && prop !== 'children') {
+        return { ...triggers, [prop]: (parameters: any) => store.assign(props[prop](parameters)) };
+      }
+
+      return triggers;
+    }, {});
+  }
+
+  componentWillMount() {
+    this._bindTriggers(this.props);
+  }
+
+  componentWillUpdate(nextProps) {
+    this._bindTriggers(nextProps);
+  }
 
   render() {
-    return <Render data={this.handleTrigger}>{this.props.children as RenderFunction<any>}</Render>;
+    return (
+      <Render data={this._triggers}>
+        {(this.props.children || this.props.render) as RenderFunction<any>}
+      </Render>
+    );
   }
 }
