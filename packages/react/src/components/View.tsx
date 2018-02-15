9b3cf7b856432ed91c61b71606ddceb9dc0e9e79
jeanfortheweb <jeanfortheweb@gmail.com>
2018-02-15 01:28:54 +0100

    Better comparison

--- packages/react/src/components/View.tsx
+++ packages/react/src/components/View.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import { Record } from 'immutable';
+import { Record, is } from 'immutable';
 import { Store, Selector } from '@memento/store';
 import Render, { RenderFunction } from './Render';
 
 export interface Props<TState extends Record<any>, TOutput> {
   store: Store<TState>;
-  selector: Selector<TState, TOutput>;
   children?: RenderFunction<TOutput>;
+  selector: Selector<TState>;
 }
 
 export interface State<TOutput = any> {
+  selector: Selector<any>;
   output?: TOutput;
 }
 
@@ -25,27 +26,47 @@ export default class View<TProps extends Props<any, any> = Props<any, any>> exte
 
   private _unsubcribe: Function = () => null;
 
+  private _updateOutput() {
+    this.setState((prevState, props) => ({
+      ...prevState,
+      output: props.store.select(this.props.selector),
+    }));
+  }
+
+  private _updateStoreListener() {
+    if (this._unsubcribe) {
+      this._unsubcribe();
+    }
+
+    this._unsubcribe = this.props.store.listen(this._updateOutput.bind(this));
+    this._updateOutput();
+  }
+
   shouldComponentUpdate(nextProps, nextState) {
-    return !(nextState.output === this.state.output);
+    return (
+      nextProps.selector !== this.props.selector ||
+      nextProps.store !== this.props.store ||
+      is(nextState.output, this.state.output) === false
+    );
   }
 
   componentWillMount() {
-    this._unsubcribe = this.props.store.listen(this.handleStoreUpdate);
+    this._updateStoreListener();
+  }
 
-    this.setState({
-      output: this.props.store.select(this.props.selector),
-    });
+  componentDidUpdate(prevProps) {
+    if (prevProps.store !== this.props.store) {
+      this._updateStoreListener();
+    }
+
+    if (prevProps.selector !== this.props.selector) {
+      this._updateOutput();
+    }
   }
 
   componentWillUnmount() {
     this._unsubcribe();
   }
-
-  handleStoreUpdate = (prevState, nextState) => {
-    this.setState({
-      output: this.props.selector(nextState),
-    });
-  };
 
   render() {
     return <Render data={this.state.output}>{this.props.children as RenderFunction<any>}</Render>;
