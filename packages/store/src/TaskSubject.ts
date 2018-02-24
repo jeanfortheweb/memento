dc1b9cf16af9546cd5f5afb2e2afc8ecc3e4b945
jeanfortheweb <jeanfortheweb@gmail.com>
2018-02-24 20:48:13 +0100

    Allow task creators to be passed to accept

--- packages/store/src/TaskSubject.ts
+++ packages/store/src/TaskSubject.ts
@@ -1,9 +1,17 @@
 import { Observable, Subject } from '@reactivex/rxjs';
 import { Task, TaskObservable } from './core';
+import { TaskCreator } from '.';
 
-class TaskSubject extends Subject<Task> implements TaskObservable, Observable<Task> {
-  public accept<TTask extends Task>(kind: TTask['kind']) {
-    return this.filter<Task, TTask>((task): task is TTask => task.kind === kind);
+class TaskSubject extends Subject<Task>
+  implements TaskObservable, Observable<Task> {
+  public accept<TTask extends Task>(
+    kind:
+      | TTask['kind']
+      | TaskCreator<TTask['kind'], TTask['payload']>,
+  ) {
+    return this.filter<Task, TTask>(
+      (task): task is TTask => task.kind === kind,
+    );
   }
 }
 
--- packages/store/src/core.ts
+++ packages/store/src/core.ts
@@ -1,7 +1,9 @@
 import { Observable } from '@reactivex/rxjs';
 import { Record } from 'immutable';
+import { TaskCreator } from '.';
 
-export interface State<TStateProps = any> extends Record<TStateProps> {}
+export interface State<TStateProps = any>
+  extends Record<TStateProps> {}
 
 export interface Updater<TState extends State> {
   (state: TState): TState;
@@ -11,23 +13,39 @@ export interface Listener<TState extends State> {
   (prevState: TState, nextState: TState): void;
 }
 
-export interface Selector<TState extends State, TOutput = any> {
+export interface Selector<
+  TState extends State,
+  TOutput = any
+> {
   (state: TState): TOutput;
 }
 
-export interface Task<TKind extends string = string, TPayload = any> {
+export interface Task<
+  TKind extends string = string,
+  TPayload = any
+> {
   kind: TKind;
   payload: TPayload;
 }
 
 export interface TaskObservable extends Observable<Task> {
-  accept<TTask extends Task>(kind: TTask['kind']): Observable<TTask>;
+  accept<TTask extends Task>(
+    kind:
+      | TTask['kind']
+      | TaskCreator<TTask['kind'], TTask['payload']>,
+  ): Observable<TTask>;
 }
 
-export interface StateObservable<TState extends State> extends Observable<TState> {
-  select<T = any>(selector: Selector<TState, T>): Observable<T>;
+export interface StateObservable<TState extends State>
+  extends Observable<TState> {
+  select<T = any>(
+    selector: Selector<TState, T>,
+  ): Observable<T>;
 }
 
 export interface Worker<TState extends State> {
-  (task$: TaskObservable, state$: StateObservable<TState>): Observable<Updater<TState> | Task>;
+  (
+    task$: TaskObservable,
+    state$: StateObservable<TState>,
+  ): Observable<Updater<TState> | Task>;
 }
