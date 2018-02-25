789746484930a200ad9148f9d241f8bcc95b3349
jeanfortheweb <jeanfortheweb@gmail.com>
2018-02-25 19:08:21 +0100

    Add info

--- packages/react-todo-example/src/components/EchoChat/Messages.jsx
+++ packages/react-todo-example/src/components/EchoChat/Messages.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Comment, Segment } from 'semantic-ui-react';
+import { Divider, Comment, Segment, Header, Icon } from 'semantic-ui-react';
 import Message from './Message';
 import { View } from '@memento/react';
 import store, { getMessages } from './store';
@@ -8,6 +8,16 @@ const Messages = () => (
   <View store={store} messages={getMessages}>
     {({ messages }) => (
       <Segment>
+        {messages.size === 0 && (
+          <Header as="h4" icon textAlign="center" disabled>
+            <Divider hidden />
+            <Icon name="info" />
+            No Messages
+            <Header.Subheader>
+              No messages found. Connect and write some!
+            </Header.Subheader>
+          </Header>
+        )}
         <Comment.Group>
           {messages.map(message => (
             <Message key={message.date.toLocaleString()} message={message} />
