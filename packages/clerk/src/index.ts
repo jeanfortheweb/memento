40818a4737a2fdf24ed7d6478428c196889eadb0
jeanfortheweb <jeanfortheweb@gmail.com>
2018-03-11 22:51:54 +0100

    Add missing exports

--- packages/clerk/src/index.ts
+++ packages/clerk/src/index.ts
@@ -1,12 +1,13 @@
 import { Worker, State } from '@memento/store';
 import { Observable } from '@reactivex/rxjs';
-import { Configuration } from './configuration';
+import { Configuration, SaveMode, LoadMode, Target, Reviver } from './configuration';
 
 import { save, SaveTask, accept as saveAccept } from './save';
 import { load, LoadTask, accept as loadAccept } from './load';
 
 export { save, SaveTask };
 export { load, LoadTask };
+export { Configuration, SaveMode, LoadMode, Target, Reviver };
 
 export default <TState extends State<TStateProps>, TStateProps extends Object>(
   configuration: Configuration,
