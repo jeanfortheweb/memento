565aaf7db7fb1dcb240d1bfccbb5cbc6e6fbf208
jeanfortheweb <jeanfortheweb@gmail.com>
2018-03-12 12:16:08 +0100

    Optimize createReviver

--- packages/clerk/src/utils.ts
+++ packages/clerk/src/utils.ts
@@ -1,4 +1,4 @@
-import { Collection, fromJS, Map } from 'immutable';
+import { Collection, fromJS } from 'immutable';
 import { Reviver } from './configuration';
 
 export const pathToArray = (path: string): (string | number)[] =>
@@ -13,30 +13,37 @@ export type SimpleReviver<T> = {
 };
 
 export type ReviverStruct<T> = {
-  [P in keyof T]: SimpleReviver<T[P]> | GeneratedReviver<T[P]>
+  [P in keyof T]?: SimpleReviver<T[P]> | GeneratedReviver<T[P]>
 };
 
 export type GeneratedReviver<T> = Reviver<T> & { _isReviver?: true };
 
+const isGeneratedReviver = (
+  input: SimpleReviver<any> | GeneratedReviver<any>,
+): input is GeneratedReviver<any> => (input as any)._isReviver === true;
+
+const revive = (
+  sequence: Collection.Keyed<string, any> | Collection.Indexed<any>,
+  reviver?: SimpleReviver<any> | GeneratedReviver<any>,
+) => {
+  if (reviver) {
+    if (isGeneratedReviver(reviver)) {
+      return fromJS(sequence.toJS(), reviver);
+    }
+
+    return reviver(sequence);
+  }
+
+  return sequence;
+};
+
 export const createReviver = <T, R>(
   rootReviver: SimpleReviver<R>,
-  struct?: ReviverStruct<Partial<T>>,
+  struct: ReviverStruct<T> = {},
 ): GeneratedReviver<R> => {
-  const map = struct ? fromJS(struct) : Map();
-
   const reviver: GeneratedReviver<R> = (key, sequence, path) => {
-    if (path && path.length > 0) {
-      const found = map.getIn(path);
-
-      if (found) {
-        if (found._isReviver) {
-          return fromJS(sequence.toJS(), found);
-        }
-
-        return found(sequence);
-      }
-
-      return sequence;
+    if (key !== '') {
+      return revive(sequence, struct[key]);
     }
 
     return rootReviver(sequence);
